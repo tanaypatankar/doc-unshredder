564dfa3ae04bf05da495a4c641cca518054e092b
Shripad <shripad1020@gmail.com>
2025-04-17 17:07:13 -0500

    Use all .jpg images for strippin'

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import os
 from matplotlib import pyplot as plt
 from shredder import shred_image
 
-def process_and_display_shreds(input_folder, image_name):
+def process_and_display_shreds(image_path):
     """
-    Reads an image from the input folder, shreds it, and displays the shreds using matplotlib.
-    
-    :param input_folder: Path to the folder containing the input image.
-    :param image_name: Name of the image file to process.
+    Shreds a single image and displays the shreds using matplotlib.
+
+    :param image_path: Full path to the image file.
     """
-    image_path = os.path.join(input_folder, image_name)
     strips = shred_image(image_path)
 
     # Display the strips using matplotlib
@@ -17,9 +15,14 @@ def process_and_display_shreds(input_folder, image_name):
     for ax, strip in zip(axes, strips):
         ax.imshow(strip)
         ax.axis('off')
+    plt.suptitle(os.path.basename(image_path))  # Optional: show image name
     plt.show()
 
 if __name__ == "__main__":
-    input_folder = "./Input"  # Replace with your input folder path
-    image_name = "image01.jpg"  # Replace with your image file name
-    process_and_display_shreds(input_folder, image_name)
+    input_folder = "./Input-Img"  # Replace with your input folder path
+
+    # Process all .jpg files in the folder
+    for filename in os.listdir(input_folder):
+        if filename.lower().endswith(".jpg"):
+            image_path = os.path.join(input_folder, filename)
+            process_and_display_shreds(image_path)
